cbc2e96c34ce4f585515dc3080d1fb5f5b01c638
Yongjae <dh179@naver.com>
2018-05-16 16:13:22 +0900

    상속 클래스 변경, Manifest 수정

--- Client/Midi/app/src/main/java/com/midi/midi/AudioApplication.java
+++ Client/Midi/app/src/main/java/com/midi/midi/AudioApplication.java
@@ -1,6 +1,7 @@
 package com.midi.midi;
 
 import android.app.Application;
+import android.os.Bundle;
 
 /**
  * Created by leejw on 2018-05-08.
@@ -8,7 +9,7 @@ import android.app.Application;
  * 앱 실행되어 Process 생성될 때 호출되는 객체
  */
 
-public class AudioApplication extends Application{
+public class AudioApplication extends GlobalApplication{
     private static AudioApplication mInstance;
     private AudioServiceInterface mInterface;
 
