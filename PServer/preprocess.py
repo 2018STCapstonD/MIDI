09cf0affff2396861f7e47d72463b6810ef75017
DESKTOP-340F6FN\ITS_1 <dh179@naver.com>
2018-07-17 16:35:37 +0900

    제목 + 앨범명 으로 해시값 생성해 아이디 부여

--- PServer/preprocess.py
+++ PServer/preprocess.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import hashlib
 
-dataframe = pd.read_csv('tempdata.csv', sep="|", encoding = 'utf8', header='infer');
+dataframe = pd.read_csv('tempdata.csv', sep="|", encoding = 'utf8', header='infer')
+
+to_hash = (dataframe["title"]+dataframe["album"])
+
+dataframe["musicID"] = to_hash.apply(hash)
 
 print(dataframe)
