225ccdcc25536d08b8c3111c05c9f3874f4a53d1
DESKTOP-340F6FN\ITS_1 <dh179@naver.com>
2018-08-01 15:10:03 +0900

    kakao_id 중복 여부 확인, 초기 데이터파일 없을 시 파일 생성 후 실행

--- PServer/preprocess.py
+++ PServer/preprocess.py
@@ -5,7 +5,13 @@ import hashlib
 tempdf = pd.read_csv('tempdata.csv', sep = "|", encoding = 'utf8', header='infer')
 
 #기존 데이터 오픈
-df = pd.read_csv('data.csv', sep = "|", encoding = 'utf8', header='infer')
+try:
+    df = pd.read_csv('data.csv', sep = "|", encoding = 'utf8', header='infer')
+except FileNotFoundError :
+    f = open("data.csv", 'a')
+    f.write("kakao_id|title|album|artist|rating|musicID")
+    f.close()
+    df = pd.read_csv('data.csv', sep = "|", encoding = 'utf8', header='infer')
 
 #제목+앨범명으로 해시값 생성해 추가.
 #환경변수 PYTHONHASHSEED = 0 꼭 설정할것!!
@@ -17,10 +23,10 @@ for row in tempdf.iterrows():
     #한줄씩 실행
     #일치하는 ID가 있으면 rating만 갱신, 없으면 데이터 추가
     to_append = row[1].to_frame().T
-    if df.loc[df['musicID'] == row[1].musicID].empty :
+    if df.loc[(df['musicID'] == row[1].musicID) & (df['kakao_id'] == row[1].kakao_id)].empty:
         df = df.append(to_append)
-    else :
-        df.loc[df['musicID'] == row[1].musicID,'rating'] = row[1].rating
+    else:
+        df.loc[(df['musicID'] == row[1].musicID) & (df['kakao_id'] == row[1].kakao_id), 'rating'] = row[1].rating
 
 #데이터 저장
-df.to_csv('data.csv', sep = "|", encoding = 'utf8', header='infer')
+df.to_csv('data.csv', sep = "|", encoding = 'utf8', header='infer', index=False)
