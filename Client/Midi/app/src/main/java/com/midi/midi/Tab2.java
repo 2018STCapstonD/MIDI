71550829007296a2041977517d892658693b8ef5
jungwon <ljw2247@gmail.com>
2018-08-03 17:18:05 +0900

    Tab2 플레이어기능 적용

--- Client/Midi/app/src/main/java/com/midi/midi/Tab2.java
+++ Client/Midi/app/src/main/java/com/midi/midi/Tab2.java
@@ -1,26 +1,121 @@
 package com.midi.midi;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 @SuppressLint("ValidFragment")
-public class Tab2 extends Fragment {
+public class Tab2 extends Fragment implements View.OnClickListener {
     Context mContext;
 
-    public Tab2(Context context){
+    private AudioAdapter mAdapter;
+    private ImageView mImgAlbumArt;
+    private TextView mTxtTitle;
+    private ImageButton mBtnPlayPause;
+
+    private Handler myHandler;
+    private Thread myThread;
+    private DBHelper dbHelper;
+
+
+    public Tab2(Context context, AudioAdapter mAdapter) {
         mContext = context;
+        this.mAdapter = mAdapter;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.activity_2,null);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_2, null);
+
+        mImgAlbumArt = (ImageView) view.findViewById(R.id.img_albumart);
+        mTxtTitle = (TextView) view.findViewById(R.id.title);
+        mBtnPlayPause = (ImageButton) view.findViewById(R.id.play_pause);
+        view.findViewById(R.id.lin_miniplayer).setOnClickListener(this);
+        view.findViewById(R.id.rewind).setOnClickListener(this);
+        mBtnPlayPause.setOnClickListener(this);
+        view.findViewById(R.id.forward).setOnClickListener(this);
+
+        registerBroadcast();
+        updateUI();
 
         return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.lin_miniplayer:
+                break;
+            case R.id.rewind:
+                AudioApplication.getmInstance().getServiceInterface().rewind();
+                break;
+            case R.id.play_pause:
+                AudioApplication.getmInstance().getServiceInterface().togglePlay();
+                break;
+            case R.id.forward:
+                AudioApplication.getmInstance().getServiceInterface().forward();
+                break;
+
+        }
+    }
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateUI();
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterBroadcast();
+    }
+
+    private void updateUI() {
+        if (AudioApplication.getmInstance().getServiceInterface().isPlaying()) {
+            mBtnPlayPause.setImageResource(R.drawable.pause);
+        } else {
+            mBtnPlayPause.setImageResource(R.drawable.play);
+        }
+        AudioAdapter.AudioItem audioItem = AudioApplication.getmInstance().getServiceInterface().getAudioItem();
+        if (audioItem != null) {
+            Uri albumArtUri = ContentUris.withAppendedId(Uri.parse("content://media/external/audio/albumart"), audioItem.mAlbumId);
+            Picasso.with(mContext).load(albumArtUri).error(R.drawable.empty_albumart).into(mImgAlbumArt);
+            mTxtTitle.setText(audioItem.mTitle);
+        } else {
+            mImgAlbumArt.setImageResource(R.drawable.empty_albumart);
+            mTxtTitle.setText("재생중인 음악이 없습니다.");
+        }
+    }
+
+    public void registerBroadcast() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BroadcastActions.PLAY_STATE_CHANGED);
+        filter.addAction(BroadcastActions.PREPARED);
+        getActivity().registerReceiver(mBroadcastReceiver, filter);
+    }
+
+    public void unregisterBroadcast() {
+        getActivity().unregisterReceiver(mBroadcastReceiver);
+    }
 }
+
