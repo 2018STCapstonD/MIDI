bbdc9bbe55f88cbd2766267e205dda74e9a86150
DESKTOP-340F6FN\ITS_1 <dh179@naver.com>
2018-08-15 16:11:47 +0900

    학습, 추천부분 분리. 음악 데이터 따로 전처리

--- PServer/preprocess.py
+++ PServer/preprocess.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import os
 
-f_path = 'C:/Users/ITS_1/Documents/MIDI/PServer'
+f_path = os.getcwd()
 #새로 받은 데이터 오픈
-tempdf = pd.read_csv('C:/Users/ITS_1/Documents/MIDI/PServer/tempdata.csv', sep = "\t", encoding = 'utf8', header='infer')
+tempdf = pd.read_csv(f_path+'/tempdata.csv', sep = "\t", encoding = 'utf8', header='infer')
 
 #기존 데이터 오픈
 try:
-    df = pd.read_csv('C:/Users/ITS_1/Documents/MIDI/PServer/data.csv', sep = "\t", encoding = 'utf8', header='infer')
+    df = pd.read_csv(f_path+'/data.csv', sep = "\t", encoding = 'utf8', header='infer')
 except FileNotFoundError :
-    f = open("C:/Users/ITS_1/Documents/MIDI/PServer/data.csv", 'a')
+    f = open(f_path+"/data.csv", 'a')
     f.write("kakao_id"+"\t"+"title"+"\t"+"album"+"\t"+"artist"+"\t"+"rating"+"\t"+"musicID")
     f.close()
-    df = pd.read_csv('C:/Users/ITS_1/Documents/MIDI/PServer/data.csv', sep = "\t", encoding = 'utf8', header='infer')
+    df = pd.read_csv(f_path+'/data.csv', sep = "\t", encoding = 'utf8', header='infer')
 
 print(tempdf.tail)
 tempdf.columns = ["kakao_id","title","album","artist","rating","musicID"]
-
 
 for row in tempdf.iterrows():
     #한줄씩 실행
@@ -27,5 +26,7 @@ for row in tempdf.iterrows():
     else:
         df.loc[(df['musicID'] == row[1].musicID) & (df['kakao_id'] == row[1].kakao_id), 'rating'] = row[1].rating
 
+musicdf = df[['title', 'artist', 'album', 'musicID']].drop_duplicates('musicID',keep='first')
 #데이터 저장
-df.to_csv('C:/Users/ITS_1/Documents/MIDI/PServer/data.csv', sep = "\t", encoding = 'utf8', header='infer', index=False)
+#df.to_csv(f_path+'/data.csv', sep = "\t", encoding = 'utf8', header='infer', index=False)
+musicdf.to_csv('musicdata.csv', sep='\t', encoding = 'utf8', header='infer', index=False)
